fd0134830a3d773ef9abde73b4932adf750db4f8
ArleySantos <98360232+ArleySantos@users.noreply.github.com>
2022-02-28 19:25:29 -0300

    conteudo

---
+++ repetições/repetições.js
@@ -0,0 +1,16 @@
+
+/*var c = 1
+
+do {
+    console.log('Tudo bem?')
+    c++
+}while(c <= 6)*/
+
+
+/*faça while(c <= 6){
+    console.log('Tudo bem?')
+    c++
+}*/
+
+
+/*console.log('Tudo bem?')*/
